aca5d96641fd6af765f2b47e22c131b41a12a0d9
amei <amei@ameideMacBook-Pro.local>
2019-12-20 22:07:31 +0800

    update

--- Chapter5/src/main/java/com/course/testng/suite/LoginTest.java
+++ Chapter5/src/main/java/com/course/testng/suite/LoginTest.java
@@ -1,4 +1,11 @@
 package com.course.testng.suite;
 
+import org.testng.annotations.Test;
+
 public class LoginTest {
+
+    @Test
+    public void loginTaoBao(){
+        System.out.println("Taobao 登录成功");
+    }
 }
