10282db24324032bdc677ce3749eea2e7f13b02c
amei <wanglmamei@gmail.com>
2019-12-22 16:33:38 +0800

    update

--- Chapter5/src/main/java/com/course/testng/multiThread/MultiThreadOnXMl.java
+++ Chapter5/src/main/java/com/course/testng/multiThread/MultiThreadOnXMl.java
@@ -5,17 +5,17 @@ import org.testng.annotations.Test;
 public class MultiThreadOnXMl {
     @Test
     public void test1() {
-        System.out.println("Thread Id : %s%n" + Thread.currentThread().getId());
+        System.out.println("Thread Id1 : " + Thread.currentThread().getId());
     }
 
     @Test
     public void test2() {
-        System.out.println("Thread Id : %s%n" + Thread.currentThread().getId());
+        System.out.println("Thread Id2 : " + Thread.currentThread().getId());
     }
 
     @Test
     public void test3() {
-        System.out.println("Thread Id : %s%n" + Thread.currentThread().getId());
+        System.out.println("Thread Id3 : " + Thread.currentThread().getId());
 
     }
 
