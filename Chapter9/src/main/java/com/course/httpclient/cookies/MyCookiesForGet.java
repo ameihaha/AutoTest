c304ff6f872f81cf69643b54d91a40701a816fb3
amei <wanglmamei@gmail.com>
2020-02-24 01:10:55 +0800

    update

--- Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -1,14 +1,17 @@
 package com.course.httpclient.cookies;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -32,11 +35,23 @@ public class MyCookiesForGet {
 
         //测试逻辑代码
         HttpGet get = new HttpGet(testUrl);
-        HttpClient client = new DefaultHttpClient();
+//        HttpClient client = new DefaultHttpClient();
+        DefaultHttpClient client = new DefaultHttpClient();
         HttpResponse response = client.execute(get);
         result = EntityUtils.toString(response.getEntity(),"utf-8");
-        System.out.println("123- amei come on ");
         System.out.println(result);
+
+
+        //获取cookies信息
+        CookieStore store =  client.getCookieStore();
+        List<Cookie> cookieList =  store.getCookies();
+
+        for (Cookie  cookie : cookieList){
+            String name =  cookie.getName();
+            String value = cookie.getValue();
+            System.out.println("cookieName = "+name +"; cookieValue = "+value);
+
+        }
     }
 
 
