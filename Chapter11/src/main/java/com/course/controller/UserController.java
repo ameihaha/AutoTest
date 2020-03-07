07e264993039080a6250c89a2548a6952df14871
彭大伟 <2066666618@qq.com>
2020-03-07 18:57:45 +0800

    myabtis demo

--- Chapter11/src/main/java/com/course/controller/UserController.java
+++ Chapter11/src/main/java/com/course/controller/UserController.java
@@ -16,11 +16,7 @@ import javax.annotation.Resource;
 @Api(value = "v1",description = "第一个版本的demo")
 @RequestMapping("v1")
 public class UserController {
-
-    //获取执行SQL语句对象
-//    @Autowired
-//    private SqlSessionTemplate template;
-
+    
     @Resource
     private UserService userservice;
 
@@ -33,23 +29,5 @@ public class UserController {
         return  result; //这里匹配resources/mapper/mysql.xml里的select标签的id
 
     }
-//    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
-//    public int addUser(@RequestBody User user){
-//        int result  = template.insert("addUser",user);
-//        return  result;
-//        return  template.insert("addUser",user);
-//    }
-
-//    @RequestMapping(value = "/updateUser",method = RequestMethod.POST)
-//    public int updateUser(@RequestBody User user){
-//        return template.update("updateUser",user);
-//
-//    }
-//    @RequestMapping(value = "/deleteUser",method = RequestMethod.POST)
-//    public int delUser(@RequestParam int id ){
-//        return  template.delete("deleteUser");
-//
-//    }
-
 
 }
