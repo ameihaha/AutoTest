8dc6f47c645ea93bfdf062be10f36628266f28ad
amei <wanglmamei@gmail.com>
2020-03-15 16:01:05 +0800

    udpate

--- Chapter11/src/main/java/com/course/service/UserService.java
+++ Chapter11/src/main/java/com/course/service/UserService.java
@@ -2,13 +2,16 @@ package com.course.service;
 
 
 import com.course.mapper.Mapper;
+import com.course.model.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     Mapper mapper;
-    public User  getUserCount(){
+    public User getUserCount(){
+
+        return  null;
 
     }
 }
