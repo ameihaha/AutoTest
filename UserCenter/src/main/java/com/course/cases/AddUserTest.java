2b66395b73a8177613d51158cf9ada2ce1dafc4c
amei <wanglmamei@gmail.com>
2020-03-15 17:12:01 +0800

    add AddUserTest

--- UserCenter/src/main/java/com/course/cases/AddUserTest.java
+++ UserCenter/src/main/java/com/course/cases/AddUserTest.java
@@ -1,4 +1,54 @@
 package com.course.cases;
 
+import com.course.config.TestConfig;
+import com.course.model.AddUserCase;
+import com.course.model.User;
+import com.course.utils.DatabaseUtil;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
 public class AddUserTest {
+
+    @Test(dependsOnGroups = "loginTrue",description = "添加用户接口")
+    public void addUser() throws IOException {
+        SqlSession session = DatabaseUtil.getSqlSession();
+        AddUserCase addUserCase =  session.selectOne("addUserCase");
+        //发请求，获取结果
+        String  result = getResult(addUserCase);
+        //验证返回结果
+        User user = session.selectOne("addUser",addUserCase);
+        Assert.assertEquals(addUserCase.getExpected(),result);
+    }
+
+    private String getResult(AddUserCase addUserCase) throws IOException {
+
+        HttpPost post = new HttpPost(TestConfig.addUserUrl);
+        JSONObject param = new JSONObject();
+        param.put("userName",addUserCase.getUserName());
+        param.put("password",addUserCase.getPassword());
+        param.put("sex",addUserCase.getSex());
+        param.put("age",addUserCase.getAge());
+        param.put("permission",addUserCase.getPermission());
+        param.put("isDelete",addUserCase.getIsDelete());
+
+        //设置头信息
+        post.setHeader("content-type","application/json");
+        StringEntity entity = new StringEntity(param.toString(),"utf-8");
+        post.setEntity(entity);
+
+        //设置cookies
+        TestConfig.defaultHttpClient.setCookieStore(TestConfig.store);
+        String result;
+        HttpResponse response = TestConfig.defaultHttpClient.execute(post);
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+        return result;
+    }
 }
